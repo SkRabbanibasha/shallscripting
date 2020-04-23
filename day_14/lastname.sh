1c49c750839031b5cc563d0374592ab340dc4f3d
SkRabbanibasha <skrabbanibasha88@gmail.com>
2020-04-23 14:10:47 +0530

    some changes to code

--- day_14/lastname.sh
+++ day_14/lastname.sh
@@ -1,9 +1,13 @@
 #! /bin/bash -
-read -p "enter a last name:" name
-pat="^[A-Za-Z]+"
+validlast(){
+name=$1
+pat="^[A-Z][a-zA-Z]{2}"
 if [[ $name =~ $pat ]]
 then
      echo valid
 else
       echo invalid
 fi
+}
+read -p "enter a last name:" lastname
+validlast $lastname
